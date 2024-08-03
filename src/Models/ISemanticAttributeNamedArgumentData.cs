70dd4049aa7c194f221bb95d9669e0736498051c
ErikWe <erikwestberg7@gmail.com>
2024-08-03 03:18:45 +0200

    Initial implementation

---
+++ src/Models/ISemanticAttributeNamedArgumentData.cs
@@ -0,0 +1,10 @@
+﻿namespace Paraminter.Arguments.Semantic.Attributes.Constructor.Models;
+
+using Microsoft.CodeAnalysis;
+
+/// <summary>Represents semantic data about an attribute constructor argument.</summary>
+public interface ISemanticAttributeNamedArgumentData
+{
+    /// <summary>The attribute constructor argument.</summary>
+    public abstract TypedConstant Argument { get; }
+}
---
+++ src/Models/README.nupkg.md
@@ -0,0 +1,5 @@
+# Paraminter.Arguments.Semantic.Attributes.Constructor.Models
+
+Defines models for `Paraminter` semantic attribute constructor argument data.
+
+See [GitHub](https://github.com/Paraminter/Paraminter.Arguments.Semantic.Attributes.Constructor) for more information.
