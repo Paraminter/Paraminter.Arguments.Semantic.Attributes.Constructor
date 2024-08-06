ccb9b8338ac30f126f3a4562f7e1a588b39d231a
ErikWe <erikwestberg7@gmail.com>
2024-08-06 14:11:17 +0200

    Derive from IArgumentData marker interface

--- src/Models/ISemanticAttributeConstructorArgumentData.cs
+++ src/Models/ISemanticAttributeConstructorArgumentData.cs
@@ -2,8 +2,11 @@
 
 using Microsoft.CodeAnalysis;
 
+using Paraminter.Associators.Models;
+
 /// <summary>Represents semantic data about an attribute constructor argument.</summary>
 public interface ISemanticAttributeConstructorArgumentData
+    : IArgumentData
 {
     /// <summary>The attribute constructor argument.</summary>
     public abstract TypedConstant Argument { get; }
